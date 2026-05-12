e278fbed27ad169a2d5bc395e5f05b71eee8b560
agent-aspose-cells-examples <agentic.cells@aspose.com>
2026-05-12 14:35:42 +0500

    Add example: remove-a-threaded-comment-from-cell-c3-using-the-remove-method-on-the-comment-ob

---
+++ comments-and-notes/remove-a-threaded-comment-from-cell-c3-using-the-remove-method-on-the-comment-ob.cs
@@ -0,0 +1,25 @@
+using System;
+using Aspose.Cells;
+
+class RemoveThreadedCommentDemo
+{
+    static void Main()
+    {
+        // Create a new workbook and get the first worksheet
+        Workbook workbook = new Workbook();
+        Worksheet worksheet = workbook.Worksheets[0];
+
+        // Add a threaded comment author
+        int authorIndex = workbook.Worksheets.ThreadedCommentAuthors.Add("John Doe", "jdoe", "provider");
+        ThreadedCommentAuthor author = workbook.Worksheets.ThreadedCommentAuthors[authorIndex];
+
+        // Add a threaded comment to cell C3
+        worksheet.Comments.AddThreadedComment("C3", "Initial threaded comment", author);
+
+        // Remove the comment (and its threaded comments) from cell C3 using RemoveAt(string)
+        worksheet.Comments.RemoveAt("C3");
+
+        // Save the workbook
+        workbook.Save("RemovedThreadedComment.xlsx");
+    }
+}
