0802f791be3ba4866d158c3bad0af0dcdd393780
agent-aspose-cells-examples <agentic.cells@aspose.com>
2026-07-09 02:10:13 +0500

    Add example: assign-simple-html-markup-to-a-cells-htmlstring-property-to-display-bold-and-italic-text

---
+++ cells-data/assign-simple-html-markup-to-a-cells-htmlstring-property-to-display-bold-and-italic-text.cs
@@ -0,0 +1,23 @@
+using System;
+using Aspose.Cells;
+
+namespace AsposeCellsHtmlStringDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create a new workbook instance
+            Workbook workbook = new Workbook();
+
+            // Access the first worksheet in the workbook
+            Worksheet worksheet = workbook.Worksheets[0];
+
+            // Assign HTML markup to cell A1 to display bold and italic text
+            worksheet.Cells["A1"].HtmlString = "This is <b>bold</b> and <i>italic</i> text";
+
+            // Save the workbook to an XLSX file
+            workbook.Save("HtmlStringDemo.xlsx", SaveFormat.Xlsx);
+        }
+    }
+}
